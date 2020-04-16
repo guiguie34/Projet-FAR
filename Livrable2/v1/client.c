44f5d9c244a88e291ceb3e6e192576f9021d7c6b
Guillaume <guillaume.chebib@hotmail.fr>
2020-04-16 18:36:18 +0200

    Optimisation v1

--- Livrable2/v1/client.c
+++ Livrable2/v1/client.c
@@ -16,42 +16,41 @@
 #include <string.h>
 #include <signal.h>
 
-int dSG;
-void fin(){
+int dSG; //variable globale contenant le descripteur de socket
+void fin(){ //fonction qui gère le cas où un message = fin
     int clo = close(dSG);
     if(clo==-1){
-		perror("error with closing client : ");
+		perror("Erreur fermeture socket ");
 		exit(1);
 	}
     exit(0);
 }
 
-void exitt(int n){
+void exitt(int n){ //gestion ctrl-c
     char *fin="fin";
 
-    sendTCP(dSG,fin,sizeof(fin),0);
+    sendTCP(dSG,fin,sizeof(fin),0); //voir le fichier sendTCP.c (valeur de retour directement gérée)
     int clo = close(dSG);
     if(clo==-1){
-		perror("error with closing client : ");
+		perror("Erreur fermeture socket ");
 		exit(1);
 	}
     exit(0);
 }
 
 void* envoi(void *data){
-    int *dS=data;
+    int *dS=data; //on recupère le descripteur de socket (avec un pointeur)
     while(1){
 
-        char saisie1[100]; //taille max du message à envoyer
+        char saisie1[100];
         memset(saisie1,0,sizeof(saisie1));
         //printf("\nsaisir le message: ");
         fgets(saisie1,100,stdin);
-        int taille1=strlen(saisie1)+1;
 
         int result = sendTCP(*dS,saisie1,sizeof(saisie1),0); //la valeur de retour est traitée dans sendTCP
-        saisie1[strlen(saisie1)-1] = '\0';
+        saisie1[strlen(saisie1)-1] = '\0'; //permet de comparer après un fgets
         if(strcmp(saisie1,"fin")==0){
-            printf("end");
+            printf("Arret du client\n");
             fin();    
         }
     }
@@ -64,17 +63,17 @@ void* recevoir(void * data){
     while(1){
 
         char rep[100];
-        int rec = recv(*dS,&rep,sizeof(rep),0);
+        int rec = recv(*dS,&rep,sizeof(rep),0); //reception du message
         if(rec==-1){
             perror("Error recv : ");
             exit(1);
         }
-        fputs(rep,stdout);
+        fputs(rep,stdout); //envoi de la réponse vers la sortie standard
         char rep2[100];
         strcpy(rep2,rep);
         rep[strlen(rep)-1] = '\0';
-        if(strcmp(rep,"fin")==0 || strcmp(rep2,"fin")==0){
-            printf("end");
+        if(strcmp(rep,"fin")==0 || strcmp(rep2,"fin")==0){ //permet verifier si le deuxième client à envoyer fin manuellement
+            printf("Arret du client\n"); //ou si il a fait ctrl-c (donc sans fgets)
             fin();
         }
         /*printf("Réponse: %s\n",rep);*/
@@ -120,10 +119,23 @@ int main(int argc, char *argv[]){
     pthread_t envo;
     pthread_t recpt;
 
-    pthread_create(&envo, NULL,envoi, (void*)&dS); 
-    pthread_create(&recpt, NULL,recevoir, (void*)&dS);
-    pthread_join(envo, NULL); 
-    pthread_join(recpt, NULL); 
+    if(pthread_create(&envo, NULL,envoi, (void*)&dS) !=0){ //creation thread
+        perror("Erreur création thread");
+        exit(1);
+    }
+    if(pthread_create(&recpt, NULL,recevoir, (void*)&dS) !=0){
+        perror("Erreur création thread");
+        exit(1);
+    }
+
+    if(pthread_join(envo, NULL) !=0){ //attente terminaison thread
+        perror("Erreur join");
+        exit(1);
+    } 
+    if(pthread_join(recpt, NULL) !=0){
+        perror("Erreur join");
+        exit(1);
+    }
     int clo = close(dS);
     if(clo==-1){
 		perror("error with closing client : ");
--- Livrable2/v1/serveur.c
+++ Livrable2/v1/serveur.c
@@ -14,21 +14,35 @@
 #include <signal.h>
 #include <string.h>
 
-int clo1;
+int clo1; //variable globales contenant les valeurs des descripteurs de sockets
 int clo2;
 int clo3;
 
-typedef struct Descripteur{
+typedef struct Descripteur{ //structure contenant les descripteurs de sockets des clients + les points vers les threads
     int dSC;
     int dSC2;
     pthread_t *one;
     pthread_t *two;
 }Descripteur;
 
-struct Descripteur *descriG;
+struct Descripteur *descriG; //variable globable qui stocke l'adresse de la structure
 
-void exitt(int n){
-    free(descriG);
+void exitt(int n){ //gestion du ctrl-c
+
+  char *fin="fin";
+  int snd= send(descriG->dSC2,fin,sizeof(fin),0); //envoi message de fin aux clients
+  if(snd == 0 || snd == -1){
+    perror("Erreur send");
+    exit(1);
+  }
+
+  snd= send(descriG->dSC,fin,sizeof(fin),0);
+  if(snd == 0 || snd == -1){
+    perror("Erreur send");
+    exit(1);
+  }
+
+  free(descriG); //libération memoire
 
   int closecli1=close(clo1);
   if(closecli1==-1){
@@ -49,42 +63,46 @@ void exitt(int n){
     perror("Error close : ");
     exit(1);
   }
-  printf("end");
+  printf("Serveur fermé");
+  exit(0);
 }
 
 void* oneTotwo(void *data){
 
-    struct Descripteur *descri = data;
+    struct Descripteur *descri = data; //on recupère la structure passée en paramètre
     char msg[100];
 
     while(1){
         memset(msg,0,sizeof(msg));
-        int rep = recv(descri->dSC,msg,sizeof(msg),0);
+        int rep = recv(descri->dSC,msg,sizeof(msg),0); //reception du message envoyé par le client1
         if(rep==-1){
 	        perror("Echec de la reception");
 	        exit(1);
         }
       
         printf("Client 1: %s\n",msg);
-        int snd= send(descri->dSC2,msg,sizeof(msg),0);
+        int snd= send(descri->dSC2,msg,sizeof(msg),0); //envoi du message au client2
         if(snd == 0 || snd == -1){
-            perror("erreur send");
+            perror("Erreur send");
             exit(1);
         }
         char msg2[100];
         strcpy(msg2,msg);
         msg[strlen(msg)-1] = '\0';
-        if(strcmp(msg,"fin")==0 || strcmp(msg2,"fin")==0){
-          printf("end");
+        if(strcmp(msg,"fin")==0 || strcmp(msg2,"fin")==0){ // gestion du cas de fin, msg stocke le message envoyé à partir de fgets (saisie manuelle par le client)
+          printf("Message de terminaison reçu\n"); //msg2 stocke le message qui n'est pas envoyé à partir de fgets (ctrl-c du client)
 	        break;
         }
     }
-    pthread_cancel(*(descri->two));
-    pthread_exit(NULL);
-
+    if(pthread_cancel(*(descri->two))!=0){
+      perror("Erreur fermture thread voisin");
+      exit(1);
+    } //fermeture du thread voisin à celui-ci
+    
+    pthread_exit(NULL); // fermeture du thread courant
 }
 
-void* twoToone(void *data){
+void* twoToone(void *data){ //fonctionnement similaire à la fonction au dessus
     struct Descripteur *descri = data;
     char msg[100];
 
@@ -99,18 +117,21 @@ void* twoToone(void *data){
         printf("Client 2: %s\n",msg);
         int snd = send(descri->dSC,msg,sizeof(msg),0);
         if(snd == 0 || snd == -1){
-            perror("erreur send");
+            perror("Erreur send");
             exit(1);
         }
         char msg2[100];
         strcpy(msg2,msg);
         msg[strlen(msg)-1] = '\0';
         if(strcmp(msg,"fin")==0 || strcmp(msg2,"fin")==0){
-          printf("end");
+          printf("Message de terminaision reçu\n");
 	        break;
         }
     }
-    pthread_cancel(*(descri->one));
+    if(pthread_cancel(*(descri->one)) !=0){
+      perror("Erreur fermeture thread voisin");
+      exit(1);
+    }
     pthread_exit(NULL);
 }
 
@@ -121,8 +142,8 @@ int main(int argc, char *argv[]){
     perror("Erreur ! Socket non créee");
     exit(1);
   }
-  if(setsockopt(dS,SOL_SOCKET,SO_REUSEADDR,&(int){1},sizeof(int)) < 0){
-    perror("setsockopt(SO_REUSEADDR) failed");
+  if(setsockopt(dS,SOL_SOCKET,SO_REUSEADDR,&(int){1},sizeof(int)) < 0){ //permet de réutiliser le port courant directement
+    perror("Erreur ! Réecriture du port échouée");
   }
   struct sockaddr_in ad;
   
@@ -152,8 +173,6 @@ int main(int argc, char *argv[]){
       exit(1);
     }
     else{
-        printf("%d\n",dSC);
-        printf("%d\n",dSC2);
         printf("Serveur en écoute ...\n");
     }
 
@@ -162,17 +181,31 @@ int main(int argc, char *argv[]){
     struct Descripteur *descri=malloc(sizeof(Descripteur));
     descriG=descri;
 
-    descri->dSC=dSC;
+    descri->dSC=dSC; //initialisation des valeurs dans la structure
     descri->dSC2=dSC2;
+    
     pthread_t one;
     pthread_t two;
     descri->one=&one;
     descri->two=&two;
-    pthread_create(&one, NULL,oneTotwo, (void*)descri);  
-    pthread_create(&two, NULL,twoToone, (void*)descri); 
-    pthread_join(one, NULL);
-    pthread_join(two, NULL);
-    free(descri);
+    
+    if(pthread_create(&one, NULL,oneTotwo, (void*)descri) !=0){
+      perror("Erreur création du thread 1");
+      exit(1);
+    } //création des threads
+    if(pthread_create(&two, NULL,twoToone, (void*)descri) !=0){
+      perror("Erreur creation du thread 2");
+      exit(1);
+    }
+    if(pthread_join(one, NULL) !=0){
+      perror("Erreur join");
+      exit(1);
+    } //attente terminaison des threads
+    if(pthread_join(two, NULL) !=0){
+      perror("Erreur join");
+      exit(1);
+    }
+    free(descri); //libération de la memoire
   }
 
   }
